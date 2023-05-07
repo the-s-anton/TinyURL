ae17ba31d18b3aa5c3fac8024419ecc2bfa2f01d
the-s-anton <antonsergeyev@me.com>
2023-05-06 20:23:09 -0400

    update sidekiq

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
+require 'sidekiq/web'
+
 Rails.application.routes.draw do
+  mount Sidekiq::Web, at: '/sidekiq'
+
   resources :urls, only: %i[index create show]
 
   get '/:shortened', to: 'urls#redirect', as: :redirect
